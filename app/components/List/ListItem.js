e97c3338f5818743e952f47765dabd22122055c5
Shankar Tiwari <shankartiwari@Shankars-MacBook-Pro.local>
2018-01-13 09:47:54 -0800

    working on navigation

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -1,96 +1,49 @@
 import React from 'react';
-import {
-  View,
-  FlatList,
-  Text,
-  TouchableOpacity,
-  AsyncStorage,
-} from 'react-native';
+import { View, FlatList, Text, TouchableOpacity } from 'react-native';
 import { List, Avatar } from 'react-native-elements';
+import PropTypes from 'prop-types';
 import NotifyIcon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
 
-const REMINDER_KEY_OBJ = 'reminderKeyObj';
 
-class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-    };
-  }
-
-  componentDidMount() {
-    AsyncStorage.getAllKeys().then((keys) => {
-      keys.forEach((element) => {
-        // console.log(element);
-        if (element !== REMINDER_KEY_OBJ) {
-          AsyncStorage.getItem(element)
-            .then(JSON.parse)
-            .then((response) => {
-              this.setState({
-                data: this.state.data.concat({
-                  key: element,
-                  title: response.inputText,
-                  date: `${response.date} ${response.time}`,
-                  duration: `Every ${response.repeatInterval} days`,
-                  notify: response.notify,
-                  avatar: 'avatar',
-                }),
-              });
-            });
-        }
-      });
-    });
-  }
-
-  render() {
-    const renderData = this.state.data;
-    return (
-      <List containerStyle={{ borderTopWidth: 1, marginTop: 0 }}>
-        <FlatList
-          data={renderData}
-          renderItem={({ item }) => (
-            <View style={styles.listContainer}>
-              <View style={styles.listLeft}>
-                <Avatar medium rounded title="MT" />
-                <View style={styles.contentDetail}>
-                  <Text style={styles.textContent}>{item.title}</Text>
-                  <Text style={styles.textContent}>{item.date}</Text>
-                  <Text style={styles.textContent}>{item.duration}</Text>
-                </View>
-              </View>
-
-              <View style={styles.icon}>
-                {item.notify === false ?
-                  <TouchableOpacity>
-                    <NotifyIcon
-                      color="white"
-                      size={20}
-                      name="notifications-off"
-                    />
-                  </TouchableOpacity> :
-                  <TouchableOpacity>
-                    <NotifyIcon
-                      color="white"
-                      size={20}
-                      name="notifications"
-                    />
-                  </TouchableOpacity>
-                }
-
-
-              </View>
+const ListItem = ({ renderData }) => (
+  <List containerStyle={{ borderTopWidth: 1, marginTop: 0 }}>
+    <FlatList
+      data={renderData}
+      renderItem={({ item }) => (
+        <View style={styles.listContainer}>
+          <View style={styles.listLeft}>
+            <Avatar medium rounded title={item.title.charAt(0)} />
+            <View style={styles.contentDetail}>
+              <Text style={styles.textContent}>{item.title}</Text>
+              <Text style={styles.textContent}>{item.date}</Text>
+              <Text style={styles.textContent}>{item.duration}</Text>
             </View>
-          )}
-          keyExtractor={item => item.key}
-          ItemSeparatorComponent={() => (
-            <View style={{ height: 0.5, backgroundColor: '#E5E5E5' }} />
-          )}
-        />
-      </List>
-    );
-  }
-}
+          </View>
+
+          <View style={styles.icon}>
+            {item.notify === false ? (
+              <TouchableOpacity>
+                <NotifyIcon color="white" size={20} name="notifications-off" />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity>
+                <NotifyIcon color="white" size={20} name="notifications" />
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+      )}
+      keyExtractor={item => item.key}
+      ItemSeparatorComponent={() => (
+        <View style={{ height: 0.5, backgroundColor: '#E5E5E5' }} />
+      )}
+    />
+  </List>
+);
+
+ListItem.propTypes = {
+  renderData: PropTypes.array.isRequired,
+};
 
 export default ListItem;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,24 +1,67 @@
-import React, { Component } from 'react';
-import { StatusBar, SafeAreaView, KeyboardAvoidingView } from 'react-native';
-import ActionButton from 'react-native-action-button';
+import React, { Component } from "react";
+import {
+  StatusBar,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  AsyncStorage
+} from "react-native";
+import ActionButton from "react-native-action-button";
 
-import { Header } from '../components/Header';
-import { ListItem } from '../components/List';
+import { Header } from "../components/Header";
+import { ListItem } from "../components/List";
+
+const REMINDER_KEY_OBJ = "reminderKeyObj";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+    this._fetchData = this._fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this._fetchData();
+  }
+
+  _fetchData = () => {
+    AsyncStorage.getAllKeys().then(keys => {
+      keys.forEach(element => {
+        // console.log(element);
+        if (element !== REMINDER_KEY_OBJ) {
+          AsyncStorage.getItem(element)
+            .then(JSON.parse)
+            .then(response => {
+              this.setState({
+                data: this.state.data.concat({
+                  key: element,
+                  title: response.inputText,
+                  date: `${response.date} ${response.time}`,
+                  duration: `Every ${response.repeatInterval} days`,
+                  notify: response.notify,
+                  avatar: "avatar"
+                })
+              });
+            });
+        }
+      });
+    });
+  };
+
   render() {
     return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#374046' }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#374046" }}>
         <StatusBar barStyle="light-content" />
         <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={10}>
           <Header />
-          <ListItem />
+          <ListItem renderData={this.state.data} />
         </KeyboardAvoidingView>
         <ActionButton
           buttonColor="rgba(231,76,60,1)"
           onPress={() =>
-            this.props.navigation.navigate('AddReminder', {
-              title: 'Create a Reminder',
+            this.props.navigation.navigate("AddReminder", {
+              title: "Create a Reminder"
             })
           }
         />
